5c91dd65bf8942c125b55cf0ee2cb0481f595d4e
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-01-30 08:55:35 -0700

    lab complete. my solution && github solution

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,97 @@
 #!/usr/bin/env python3
 
+# def admin_login(username, password):
+#     if (username == "admin" or username == "ADMIN") and (password == "12345"):
+#         return "Access granted"
+#     else:
+#         "Access denied"
+
+# def hows_the_weather(temperature):
+#     if temperature < 40:
+#         return "It's brisk out there!"
+#     elif temperature >= 40 and temperature < 65:
+#         return "It's a little chilly out there!"
+#     elif 85 >= temperature:
+#         return "It's too dang hot out there!"
+    
+#     print("It's perfect out there!")
+        
+    
+
+# def fizzbuzz(num):
+#     if not num % 15:
+#         return "FizzBuzz"
+#     elif not num % 3:
+#         return "Fizz"
+#     elif not num % 5:
+#         return "Buzz"
+    
+    
+#     return num
+
+# def calculator(operation, num1, num2):
+#     if operation == "+":
+#         return num1 + num2
+#     elif operation == "-":
+#         return num1 - num2
+#     elif operation == "*":
+#         return num1 * num2
+#     elif operation == "/":
+#         return num1 / num2
+    
+#     print("Invalid operation!")
+#     return None
+
+# # # OR...
+# def calculator(operation, num1, num2):
+#     if operation is "+":
+#         return num1 + num2
+#     elif operation is "-":
+#         return num1 - num2
+#     elif operation is "*":
+#         return num1 * num2
+#     elif operation is "/":
+#         return num1 / num2
+    
+#     print("Invalid operation!")
+#     return None
+
+
 def admin_login(username, password):
-    # your code here
-    pass
+    if username.upper() == "ADMIN" and password == "12345":
+        return "Access granted"
+    
+    return "Access denied"
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature > 85:
+        return "It's too dang hot out there!"
+    elif 65 >= temperature >= 40:
+        return "It's a little chilly out there!"
+    elif temperature < 40:
+        return "It's brisk out there!"
+
+    return "It's perfect out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if not num % 15:
+        return "FizzBuzz"
+    elif not num % 5:
+        return "Buzz"
+    elif not num % 3:
+        return "Fizz"
+    
+    return num
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+
+    print("Invalid operation!")
+    return None
